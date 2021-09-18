09bb6ef943b739412401c591bfc66c66137cbdc8
unknown <liwenxiao1@360buyad.local>
2021-09-18 13:20:27 +0800

    push gitops code

---
+++ constant.py
@@ -0,0 +1,9 @@
+####sample gitops vela test
+
+GIT_USER = "liwenxiao"
+
+GIT_PASS = "lwx@tpaas"
+
+GIT_INFO = "Hello World!"
+
+
---
+++ utils.py
@@ -0,0 +1,12 @@
+####sample gitops vela test
+from constant import *
+
+
+def GitOps():
+
+    print('###############GitOps Testing')
+    print('git user: %s' % (GIT_USER))
+    print('git pass: %s' % (GIT_PASS))
+    print('git info %s' % (GIT_INFO)) 
+
+GitOps()
